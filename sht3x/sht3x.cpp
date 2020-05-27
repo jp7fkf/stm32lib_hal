a4b5ec32b7323679ae3db18e92a66ee52bae4fcc
jp7fkf <jp7fkf@gmail.com>
2020-05-28 03:49:06 +0900

    add sht3x libs

---
+++ sht3x/sht3x.cpp
@@ -0,0 +1,60 @@
+/*
+ * sht3x.cpp
+ *
+ *  Created on: May 28, 2020
+ *      Author: Yudai Hashimoto
+ *      Web   : https://jp7fkf.dev/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Yudai Hashimoto (JP7FKF)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "sht3x.hpp"
+
+Sht3x::Sht3x(I2C_HandleTypeDef hi2cx){
+  this->hi2cx_ = hi2cx;
+}
+
+void Sht3x::GetTempeatureHumiditySingleShot(float *temp, float *humid){
+  uint8_t rxbuf[6];
+  uint8_t  settings[] = {0x2C, 0x06};
+
+  HAL_I2C_Master_Transmit(&hi2cx_, (0x45<<1)|0x00, settings, 2, SHT_I2C_TIMEOUT);
+  HAL_I2C_Master_Receive(&hi2cx_, (0x45<<1)|0x01, rxbuf, 6, SHT_I2C_TIMEOUT);
+
+  if (temp != NULL)
+    *temp = this->calculateTempeature(rxbuf[0], rxbuf[1]);
+  if (humid != NULL)
+    *humid = this->calculateHumidity(rxbuf[3], rxbuf[4]);
+}
+
+float Sht3x::calculateTempeature(uint8_t msb, uint8_t lsb){
+  uint16_t val;
+  val = msb << 8 | lsb;
+  return (float)val * 175 / 65535 - 45;
+}
+
+float Sht3x::calculateHumidity(uint8_t msb, uint8_t lsb){
+  uint16_t val;
+  val = msb << 8 | lsb;
+  return (float)val / 65535 * 100;
+}
---
+++ sht3x/sht3x.hpp
@@ -0,0 +1,52 @@
+/*
+ * sht3x.cpp
+ *
+ *  Created on: May 28, 2020
+ *      Author: Yudai Hashimoto
+ *      Web   : https://jp7fkf.dev/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Yudai Hashimoto (JP7FKF)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SHT3X_HPP_
+#define SHT3X_HPP_
+
+#include "stm32f0xx_hal.h"
+
+constexpr uint8_t  SHT_I2C_ADDR0         = 0x44;
+constexpr uint8_t  SHT_I2C_ADDR1         = 0x45;
+constexpr uint8_t  CLOCK_STRETCH_ENABLE  = 0x2C;
+constexpr uint8_t  CLOCK_STRETCH_DISABLE = 0x24;
+constexpr unsigned int SHT_I2C_TIMEOUT   = 1000; //timeout value of I2C transmit API
+
+class Sht3x {
+  public:
+    Sht3x(I2C_HandleTypeDef hi2cx);
+    void GetTempeatureHumiditySingleShot(float *temp, float *humid);
+  private:
+    I2C_HandleTypeDef hi2cx_;
+    float calculateTempeature(uint8_t msb, uint8_t lsb);
+    float calculateHumidity(uint8_t msb, uint8_t lsb);
+};
+
+#endif /* SHT3X_HPP_ */
