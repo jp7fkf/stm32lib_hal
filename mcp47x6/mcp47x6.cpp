15f6f05a76ff95b8c7d9fbf1b1010447d4db7d3a
jp7fkf <jp7fkf@gmail.com>
2020-05-29 01:05:02 +0900

    add mcp47x6 dac libs

---
+++ mcp47x6/mcp47x6.cpp
@@ -0,0 +1,42 @@
+/*
+ * mcp47x6.cpp
+ *
+ *  Created on: May 29, 2020
+ *      Author: Yudai Hashimoto
+ *      Web   : https://jp7fkf.dev/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Yudai Hashimoto (JP7FKF)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "mcp47x6.hpp"
+
+Mcp47x6::Mcp47x6(I2C_HandleTypeDef hi2cx){
+  this->hi2cx_ = hi2cx;
+}
+
+void Mcp47x6::SetDacOut(uint16_t val) {
+  uint8_t buf[2];
+  buf[0] = (val >> 8) & 0x0F;
+  buf[1] = val & 0xFF;
+  HAL_I2C_Master_Transmit(&hi2cx_, (0x60<<1)|0x00, buf, 2, MCP47x6_I2C_TIMEOUT);
+}
---
+++ mcp47x6/mcp47x6.hpp
@@ -0,0 +1,46 @@
+/*
+ * mcp47x6.hpp
+ *
+ *  Created on: May 29, 2020
+ *      Author: Yudai Hashimoto
+ *      Web   : https://jp7fkf.dev/
+ *
+ * MIT License
+ *
+ * Copyright (c) 2020 Yudai Hashimoto (JP7FKF)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_MCP47X6_HPP_
+#define SRC_MCP47X6_HPP_
+
+#include "stm32f0xx_hal.h"
+
+constexpr unsigned int MCP47x6_I2C_TIMEOUT   = 1000; //timeout value of I2C transmit API
+
+class Mcp47x6 {
+  public:
+    Mcp47x6(I2C_HandleTypeDef hi2cx);
+    void SetDacOut(uint16_t val);
+  private:
+    I2C_HandleTypeDef hi2cx_;
+};
+
+#endif /* SRC_MCP47X6_HPP_ */
